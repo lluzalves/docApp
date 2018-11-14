b955f9cf09d94982c74f914eab20ddc7072c0d3c
Daniel Luz Alves <danielluz.alves@outlook.com>
2018-11-13 23:14:46 -0200

    Add layouts    - Login    - Add documents when empty

--- app/src/main/java/com/app/daniel/ifdoc/commons/application/App.kt
+++ app/src/main/java/com/app/daniel/ifdoc/commons/application/App.kt
@@ -2,7 +2,7 @@ package com.app.daniel.ifdoc.commons.application
 
 import android.app.Application
 
-class IfDocsApp :Application(){
+class App :Application(){
 
 
 }
--- app/src/main/java/com/app/daniel/ifdoc/ui/MainActivity.kt
+++ app/src/main/java/com/app/daniel/ifdoc/ui/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
     }
 
     fun pullData() {
